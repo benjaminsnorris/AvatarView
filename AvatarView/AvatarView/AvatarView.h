4cd74b36a5d963a46f55e4b7d989bd2d6e09163f
Ben Norris <ben@bsn.io>
2016-06-01 17:07:45 -0600

    Add empty framework

---
+++ AvatarView/AvatarView/AvatarView.h
@@ -0,0 +1,19 @@
+//
+//  AvatarView.h
+//  AvatarView
+//
+//  Created by Ben Norris on 6/1/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AvatarView.
+FOUNDATION_EXPORT double AvatarViewVersionNumber;
+
+//! Project version string for AvatarView.
+FOUNDATION_EXPORT const unsigned char AvatarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AvatarView/PublicHeader.h>
+
+
