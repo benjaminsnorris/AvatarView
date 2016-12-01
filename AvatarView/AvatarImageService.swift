a4e13d5146f43f8d69d32e5fb982915e1980e90d
Ben Norris <code@bsn.design>
2016-12-01 11:06:32 -0700

    Add support for picking/taking image

--- AvatarView/AvatarImageService.swift
+++ AvatarView/AvatarImageService.swift
@@ -7,43 +7,49 @@
 
 import UIKit
 
-public protocol AvatarImageServiceDelegate {
-    
+public protocol AvatarImageServiceDelegate: class {
+    func updateImage(with image: UIImage?)
 }
 
-public struct AvatarImageService {
+open class AvatarImageService: NSObject {
     
-    // MARK: - Public properties
+    // MARK: - Internal properties
     
-    public var delegate: AvatarImageServiceDelegate?
+    let viewController: UIViewController
+    let delegate: AvatarImageServiceDelegate
     
     
     // MARK: - Initializers
     
-    public init() { }
+    required public init(viewController: UIViewController, delegate: AvatarImageServiceDelegate) {
+        self.viewController = viewController
+        self.delegate = delegate
+        super.init()
+    }
     
     
     // MARK: - Public functions
     
-    public func presentImageOptions(from viewController: UIViewController, sender: Any?, existingPhoto: Bool = false, tintColor: UIColor? = nil) {
+    public func presentImageOptions(from sender: Any?, existingPhoto: Bool = false, tintColor: UIColor? = nil) {
         let actionSheet = UIAlertController(title: NSLocalizedString("Avatar image options", comment: "Name of action sheet with options to edit avatar image"), message: nil, preferredStyle: .actionSheet)
         if let tintColor = tintColor {
             actionSheet.view.tintColor = tintColor
         }
-        actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Use last photo", comment: "Action title to use most recent photo in library"), style: .default) { _ in
-            // Grab last photo
-        })
-        actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Take photo", comment: "Action title to take a new photo"), style: .default) { _ in
-            // Take photo
-        })
-        actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Choose from library", comment: "Action title to take a new photo"), style: .default) { _ in
-            // Choose photo
-        })
-        let removeAction = UIAlertAction(title: NSLocalizedString("Remove photo", comment: "Action title to remove photo"), style: .destructive) { _ in
-            // Remove photo
+//        actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Use last photo", comment: "Action title to use most recent photo in library"), style: .default) { _ in
+//            // TODO: Grab last photo
+//        })
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Take photo", comment: "Action title to take a new photo"), style: .default) { _ in
+                self.pickImage(fromLibrary: false)
+            })
         }
-        if !existingPhoto {
-            actionSheet.addAction(removeAction)
+        actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Choose from library", comment: "Action title to take a new photo"), style: .default) { _ in
+            self.pickImage(fromLibrary: true, sender: sender)
+        })
+        if existingPhoto {
+            actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Remove photo", comment: "Action title to remove photo"), style: .destructive) { _ in
+                self.delegate.updateImage(with: nil)
+            })
         }
         actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel button title"), style: .cancel, handler: nil))
         if let barButtonItem = sender as? UIBarButtonItem {
@@ -53,6 +59,52 @@ public struct AvatarImageService {
             actionSheet.popoverPresentationController?.sourceRect = view.frame
         }
         viewController.present(actionSheet, animated: true, completion: nil)
+    }
+    
+}
+
+
+// MARK: - Image picker controller delegate
+
+extension AvatarImageService: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        defer { picker.dismiss(animated: true, completion: nil) }
+        if picker.sourceType == .camera, let originalImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            UIImageWriteToSavedPhotosAlbum(originalImage, nil, nil, nil)
+        }
+        guard let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        delegate.updateImage(with: editedImage)
+    }
+    
+}
+
+
+// MARK: - Private functions
+
+private extension AvatarImageService {
+    
+    func pickImage(fromLibrary: Bool, sender: Any? = nil) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        if fromLibrary {
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.modalPresentationStyle = .popover
+            if let barButtonItem = sender as? UIBarButtonItem {
+                imagePicker.popoverPresentationController?.barButtonItem = barButtonItem
+            } else if let view = sender as? UIView {
+                imagePicker.popoverPresentationController?.sourceView = view.superview
+                imagePicker.popoverPresentationController?.sourceRect = view.frame
+            }
+        } else {
+            imagePicker.sourceType = .camera
+            imagePicker.cameraDevice = .front
+            imagePicker.modalPresentationStyle = .fullScreen
+        }
+        
+        viewController.present(imagePicker, animated: true, completion: nil)
     }
     
 }
